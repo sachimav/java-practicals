49587205e55a45e4760b4c8b1664cda6c25be3bb
Sachintha Tissera <thiserasachintha@gmail.com>
2025-04-12 12:52:08 +0530

    Add files via upload

---
+++ s3/a/Test.java
@@ -0,0 +1,12 @@
+package a;
+
+public class Test{
+	public void print(){
+		System.out.println("hi from Test in packge a");
+	}
+	
+	public void display(){
+		System.out.println("hi from Test in packge a Display");
+	}
+	
+}
---
+++ s3/b/Test.java
@@ -0,0 +1,7 @@
+package b;
+
+public class Test{
+	public void print(){
+		System.out.println("hi from Test in packge b");
+	}
+}
